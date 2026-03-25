cd4616d0e51dff11c78747a1353f66376e11c357
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-03-25 18:13:54 +0600

    Bismillah

--- screen/page/rnr-component.tsx
+++ screen/page/rnr-component.tsx
@@ -7,22 +7,19 @@ import {Icon} from "@/components/ui/icon";
 import {ChevronRight} from "lucide-react-native";
 import {View} from "react-native";
 
-const DATA = [
-  { id: "1", title: "Item One", subtitle: "This is item one" },
-  { id: "2", title: "Item Two", subtitle: "This is item two" },
-  { id: "3", title: "Item Three", subtitle: "This is item three" },
+const componentList = [
+    {name: "Card"},
 ];
 
 export default function RnrComponent() {
   return (
     <View className="flex-1 bg-background px-4 pt-3">
-      <Text className="text-2xl font-bold mb-5">
-        My List
+      <Text className="text-lg font-bold pb-4">
+        Component List
       </Text>
 
         <FlashList
-            data={DATA}
-            keyExtractor={(item) => item.id}
+            data={componentList}
             showsVerticalScrollIndicator={false}
             renderItem={({item, index}) => (
                 <Button
@@ -32,12 +29,11 @@ export default function RnrComponent() {
                     className={cn(
                         'dark:bg-background border-border flex-row justify-between rounded-none border-b-0 pl-4 pr-3.5',
                         index === 0 && 'rounded-t-lg',
-                        index === DATA.length - 1 && 'rounded-b-lg border-b'
+                        index === componentList.length - 1 && 'rounded-b-lg border-b'
                     )}>
                     <View>
-                        <Text className="text-base font-normal">{item.title}</Text>
+                        <Text className="text-base font-normal">{item.name}</Text>
                     </View>
-
                     <Icon as={ChevronRight} className="text-muted-foreground size-4 stroke-[1.5px]"/>
                 </Button>
             )}
