b843742c1090ee30905e419325a057c5b50184f1
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-03-25 14:30:35 +0600

    Bismillah

--- App.tsx
+++ App.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {StatusBar} from 'expo-status-bar';
-import {NavigationContainer} from "@react-navigation/native";
+import {NavigationContainer, ThemeProvider} from "@react-navigation/native";
 import RootNavigator from "./screen/other/root-navigator";
-import "./global.css";
+import '@/global.css';
+import {NAV_THEME} from "@/lib/theme";
+import {useColorScheme} from "nativewind";
+import { PortalHost } from "@rn-primitives/portal";
 
 export default function App() {
+    const { colorScheme } = useColorScheme();
     return (
-        <GestureHandlerRootView style={{flex: 1}}>
-            <StatusBar style="dark"/>
-            <NavigationContainer>
-                <RootNavigator/>
-            </NavigationContainer>
-        </GestureHandlerRootView>
+        <ThemeProvider value={NAV_THEME[colorScheme ?? 'light']}>
+            <GestureHandlerRootView style={{flex: 1}}>
+                <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+                <NavigationContainer>
+                    <RootNavigator/>
+                </NavigationContainer>
+                <PortalHost />
+            </GestureHandlerRootView>
+        </ThemeProvider>
     );
 }
