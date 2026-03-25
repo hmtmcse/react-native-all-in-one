4799bc0ec1ff3da1ef7039bc074993f9c69ec3f3
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-03-25 15:48:56 +0600

    Bismillah

--- screen/page/home.tsx
+++ screen/page/home.tsx
@@ -1,5 +1,6 @@
 import { View, Text } from 'react-native';
 import {useColorScheme} from "nativewind";
+import {AccordionPreview} from "./accordion-preview";
 
 export default function Home() {
     const {colorScheme} = useColorScheme();
@@ -8,6 +9,7 @@ export default function Home() {
             <Text>
                 Home Page {colorScheme}
             </Text>
+            <AccordionPreview/>
         </View>
     );
 }
